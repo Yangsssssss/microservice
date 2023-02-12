adbb1d0bb0b21a977c42f22fb1bab75a9028be04
sheny <sheny@jztdata.com>
2023-02-12 21:18:35 +0800

    feat: 搭建基础 maven 项目
    搭建可启动的 spring 项目，base 模块为基础模块为其他模块提供工具类以及一些基础功能，main 模块为该项目的启动类。

---
+++ main/src/main/java/com/yangs/main/MainApplication.java
@@ -0,0 +1,18 @@
+package com.yangs.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: 启动主函数
+ * @Author: shenyang
+ * @Date: 2023/2/12
+ */
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+        System.out.println("................................................................");
+    }
+}
